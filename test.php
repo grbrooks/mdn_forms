16f3820a4eee7dc71b3ec0016c258e417bd980d9
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-14 11:01:30 +0100

    --- Auto-Git Commit ---

--- test.php
+++ test.php
@@ -3,7 +3,10 @@
  $name = $_POST['user_name'];
  $email_address = $_POST['user_email'];
  $message=$_POST['user_message'];
+ $date=date('d/m/y');
  echo "<h2>Your name: $name</h2><br>";
 echo "<h2>Your Email: $email_address<br></h2>";
  echo"<h3>Message :$message</h3>";
- echo "<h3></h3>date("Y-m-d H");
+ echo "<h2>$date</h2>";
+
+
