e4f855b562f59724ee1a259842e57373a057ba50
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-14 14:10:38 +0100

    --- Auto-Git Commit ---

--- handler.php
+++ handler.php
@@ -1,12 +1,3 @@
+<?php
 
-<?php 
-require_once 'QuickForm.php';
- $name = $_POST['user_name'];
- $email_address = $_POST['user_email'];
- $message=$_POST['user_message'];
- echo "<h2>Your name: $name</h2><br>";
- if(empty($_POST['email_address'])){
-     echo 'An email required';
- }else{
-     echo <h2>$email_address</h2>
- }
+?>
