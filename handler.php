73bf02be4ca5359796559f6ae344fec5dfa0e8c4
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-14 14:40:56 +0100

    --- Auto-Git Commit ---

--- handler.php
+++ handler.php
@@ -1,3 +1,15 @@
 <?php
 
+if(empty($_POST['user_name'])){
+    echo 'An Name required<br>';
+}else{
+$nme= htmlspecialchars($_POST['user_name']);
+}
+if(empty($_POST['user_email'])){
+    echo 'An Email required';
+}else{
+$eml= htmlspecialchars($_POST['user_email']);
+}
+echo "$nme<br>";
+echo $eml;
 ?>
