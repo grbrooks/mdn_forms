f712a1a45decec81f29c720d4c9bb69f76a540e0
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-14 10:00:52 +0100

    --- Auto-Git Commit ---

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 
 <?php 
-$email_address = $_POST['user_email'];
+ $name = $_POST['user_name'];
+
+
+ $email_address = $_POST['user_email'];
+ echo "<h2>Your name: $name</h2><br>";
+ 
+echo "<h2>Your Email: $email_address<br></h2>";
+ echo"<P>Message :$msg</P>
+
 
 
 ?>
