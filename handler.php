c42b42fcbabbf662a21d1a22473f966a1974df84
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-19 11:02:02 +0100

    undefined

--- handler.php
+++ handler.php
@@ -1,31 +1,25 @@
 <?php
+$email=($_POST['user_email']);
+echo $email;
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
 if(empty($_POST['user_name'])){
-    echo '<h2>An Name required<br></h2>';
+    echo '<h2>A Name required<br></h2>';
 }else{
-$nme= htmlspecialchars($_POST['user_name']);
+$nme= ($_POST['user_name']);
 echo "<h2>Your name: $nme</h2><br>";
 }
 
-if(empty($_POST['user_email'])){
-    echo '<h2>An Email required</h2>';
-   
-}else{
-$eml=$_POST['user_email'];
-if(!filter_var($eml,FILTER_VALIDATE_EMAIL)){
-    echo '<h2>Email must be a valid email address</h2><br>';
 
-}
-
-
-}if(empty($_POST['user_message'])){
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("<h2>$email is a valid email address</h2>");
+  } else {
+    echo("<h2>$email is not a valid email address</h2>");
+  }
+if(empty($_POST['user_message'])){
     echo '<h2>A message required</h2>';
    
 }else{
-$msg= htmlspecialchars($_POST['user_message']);
+$msg=($_POST['user_message']);
 echo "<h2>Your message: $msg</h2>";
-
 }
-
-
-?>
---
+++ quickform.php
@@ -0,0 +1,5 @@
+
+<?php
+$str = "This is some <b>bold</b> text.";
+echo htmlspecialchars($str);
+?>
